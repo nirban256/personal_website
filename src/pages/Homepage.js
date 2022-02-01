c25fb3dc4f5cacd8ea952da23709e0dda59ed15d
nirban256 <nirban256@gmail.com>
2022-02-01 13:12:11 +0530

    corrected typos

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = () => {
                     <span> Nirban Chakraborty</span>
                 </h1>
                 <p>
-                    I am a Frontend Web Developer, an Open Source Enthusiast, and a blockchain explorer from West Bengal, India. If you are here feel free to say hi on <a href="https://twitter.com/NirbanChakrab10" target="_blank" rel='noreferrer'>Twitter</a>
+                    I am a Frontend Web Developer, an Open Source Enthusiast, and a blockchain explorer from West Bengal, India. Feel free to say a hi to me on <a href="https://twitter.com/NirbanChakrab10" target="_blank" rel='noreferrer'>Twitter</a>
                 </p>
             </header>
         </div>
