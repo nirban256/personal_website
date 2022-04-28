8c70f3a4273679e110d4b8ae27a86c1cbad470d7
nirban256 <nirban256@gmail.com>
2022-04-28 11:21:18 +0530

    removed web vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
