d41eb01abb0c7f5659d6c863e6788a15eebb7443
NIRBAN CHAKRABORTY <nirban256@gmail.com>
2023-12-20 16:25:00 +0530

    updated new resume link and general information

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -9,7 +9,7 @@ const ImageSection = () => {
             </div>
             <div className="about-info">
                 <p className='about-text'>
-                    I am a Frontend Web3 Developer from West Bengal, India. I am studying my Bachelor's in Commputer Application from Siliguri College while looking for opportunities in the field of Web development. Currently I am learning Solidity and Web3 in order to become a full stack block chain developer.
+                    I am a Frontend Web3 Developer from West Bengal, India. I am studying my Master's in Commputer Application from VIT Bhopal while looking for opportunities in the field of Blockchain development. Currently I am learning Solidity and Web3 in order to become a full stack block chain developer.
                 </p>
 
                 <div className="about-details">
@@ -22,14 +22,14 @@ const ImageSection = () => {
                     </div>
                     <div className="right-section">
                         <p>: <span>Nirban Chakraborty</span></p>
-                        <p>: 19</p>
+                        <p>: 21</p>
                         <p>: Indian</p>
                         <p>: Bengali, Hindi, English</p>
                         <p>: Siliguri, West Bengal, India</p>
                     </div>
                 </div>
                 <button className="btn">
-                    <a href="https://rxresu.me/nirban256/myresume" target="_blank" rel="noreferrer">My Resume</a>
+                    <a href="https://drive.google.com/file/d/18tbF902M_AyUf-hhL0ihx43_Jh77bd_4/view?usp=drive_link" target="_blank" rel="noreferrer">My Resume</a>
                 </button>
             </div>
         </div>
